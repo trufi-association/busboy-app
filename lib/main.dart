5da36bfbd68ab5ee76a20bc90bc3f4eb297b4363
Gustavo TIto <42806689+GustavoTCh@users.noreply.github.com>
2022-04-07 08:09:37 -0400

    [feature] configure busboy app settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trufi_core/base/blocs/map_configuration/map_configuration_cubit.dart';
+import 'package:trufi_core/base/blocs/map_tile_provider/map_tile_provider.dart';
 import 'package:trufi_core/base/utils/graphql_client/hive_init.dart';
 import 'package:trufi_core/base/widgets/drawer/menu/social_media_item.dart';
 import 'package:trufi_core/default_values.dart';
@@ -12,34 +13,41 @@ void main() async {
   await initHiveForFlutter();
   runApp(
     TrufiApp(
-      appNameTitle: 'ExampleApp',
+      appNameTitle: 'Busboy',
       blocProviders: [
         ...DefaultValues.blocProviders(
-          otpEndpoint: "https://cbba.trufi.dev/otp",
+          otpEndpoint: "https://otp.busboy.app/otp/routers/default",
           otpGraphqlEndpoint: "https://cbba.trufi.dev/otp/index/graphql",
           mapConfiguration: MapConfiguration(
-            center: LatLng(-17.392600, -66.158787),
+            center: LatLng(5.82303, -73.03762),
           ),
           searchAssetPath: "assets/data/search.json",
           photonUrl: "https://cbba.trufi.dev/photon",
+          mapTileProviders: [
+            OSMMapLayer(
+              mapTilesUrl:
+                  'https://otp.busboy.app/maps/osm-bright/{z}/{x}/{y}@2x.jpg',
+            )
+          ],
         ),
       ],
       trufiRouter: TrufiRouter(
         routerDelegate: DefaultValues.routerDelegate(
-          appName: 'ExampleApp',
+          appName: 'Busboy',
           cityName: 'City',
-          countryName: 'Country',
+          countryName: 'Colombia',
           backgroundImageBuilder: (_) {
             return Image.asset(
               'assets/images/drawer-bg.jpg',
               fit: BoxFit.cover,
             );
           },
-          urlFeedback: 'https://example/feedback',
-          emailContact: 'example@example.com',
+          urlFeedback:
+              'https://trufifeedback.z15.web.core.windows.net/route.html',
+          emailContact: 'feedback@trufi.app',
           urlShareApp: 'https://example/share',
           urlSocialMedia: const UrlSocialMedia(
-            urlFacebook: 'https://www.facebook.com/Example',
+            urlFacebook: 'https://www.facebook.com/trufiapp/',
           ),
         ),
       ),
